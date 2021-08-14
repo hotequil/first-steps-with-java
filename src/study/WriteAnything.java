fd20adb004dd9b959516a8c6671184ebd6d25e2e
hotequil <joaohotequil@gmail.com>
2021-08-14 14:07:54 -0300

    adicionado nome do usuario na pergunta do scanner

--- src/study/WriteAnything.java
+++ src/study/WriteAnything.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 public class WriteAnything {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		String name;
 		int value;
 		
-		System.out.print("Informe um inteiro:");
+		System.out.print("Digite seu nome: ");
+		name = scanner.nextLine();
+		
+		System.out.print("Informe um inteiro: ");
 		value = scanner.nextInt();
 		
 		switch(value) {
 			case 1:
-				System.out.println("O inteiro é 1");
+				System.out.println(name + ", você digitou o inteiro 1");
 				break;
 			default:
-				System.out.println("O inteiro não é 1, mas é " + value);
+				System.out.println(name + ", você não digitou o inteiro 1, mas sim o " + value);
 		}
 		
 		scanner.close();
