c1b5bf2496e19b52d95c575e1bc409d42ce2becc
hotequil <joaohotequil@gmail.com>
2021-08-18 21:05:22 -0300

    Adicionada class de Sum

---
+++ src/study/Sum.java
@@ -0,0 +1,12 @@
+package study;
+
+public class Sum {
+	public static void main(String[] args) {
+		System.out.println(sum(19, 20));
+		System.out.println(sum(86, 35));
+	}
+	
+	public static double sum(double a, double b) {
+		return a + b;
+	}
+}
